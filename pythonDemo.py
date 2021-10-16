096e9db79fbf5e445f51b2e4b7a5c1e879e5e833
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2021-10-16 10:16:46 +0500

    Lecture 2
    Python concepts - continue

--- pythonDemo.py
+++ pythonDemo.py
@@ -1,7 +1,34 @@
 # print("Hello World")
-x = 5
-y = "Jahanzeb"
+# x = 5
+# y = "Jahanzeb"
 
-print(type(y))
+# print(type(y))
 
-print(y[6])
+# print(y[6])
+
+# for x in "banana":
+#   print(x)
+
+# b = "Hello, World!"
+# print(b.split(","))
+
+# thislist = ["apple", "banana", "cherry"]
+# for x in thislist:
+#     print(x)
+
+
+# thislist = ["apple", "banana", "cherry"]
+# for i in range(len(thislist)):
+#     print(thislist[i])
+
+str = "Jahanzeb Naeem"
+# def reverse_string(str):
+str1 = ""   # Declaring empty string to store the reversed string
+for i in str:
+    str1 = i + str1
+# return str1    # It will return the reverse string to the caller function
+print(str1)
+
+# str = "Jahanzeb Naeem"    # Given String
+# print("The original string is: ", str)
+# print("The reverse string is", reverse_string(str))  # Function call
